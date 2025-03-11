1f105bbe40125f242c53a62a6155aa11e816e46e
DemonQAQ <30318037+DemonQAQ@users.noreply.github.com>
2025-03-12 03:34:46 +0800

    Update CatalogueData.java

--- src/main/java/yy/guidemenu/datamodule/CatalogueData.java
+++ src/main/java/yy/guidemenu/datamodule/CatalogueData.java
@@ -4,11 +4,8 @@ import java.util.ArrayList;
 
 public class CatalogueData
 {
-    public String catalogueName;//用来区分不同教程分类
-    public ArrayList<GuideData> guides;//用来存储读取进来的GuideData对象
-
-    //这个位置的两个数据是用来切换分类的图标图片的 所以icon里的数据条数是固定的
-    //直接用String储存
+    public String catalogueName;
+    public ArrayList<GuideData> guides;
     public String defaultPath;
     public String checkPath;
 
